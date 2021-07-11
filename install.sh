eade578588579a43797783e70dee6d76f6a58f1d
Javier Rivas <javier.rivas@devo.com>
2021-07-11 13:40:51 +0200

    delete braces from if statement

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ dir=~/dotfiles
 # Create workspace
 echo "create workspace"
 cd ~
-if [ ! -d ~/workspace ]; then
+if ! -d ~/workspace; then
     mkdir ~/workspace
 fi
 
 echo "SECTION -> Homebrew ( package management )"
 # Install Homebrew ( package management )
-if [ ! hash brew 2>/dev/null ]; then
+if ! hash brew 2>/dev/null; then
     echo "Installing homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi    
 echo "Installing Homebrew packages"
-if [ hash git 2>/dev/null ] && [ hash brew 2>/dev/null ]; then
+if hash git 2>/dev/null && hash brew 2>/dev/null; then
     echo "Clone servo repo"
     git clone https://github.com/servo/servo.git
     cd servo
@@ -30,7 +30,7 @@ else
 fi    
 echo "SECTION -> Dotfiles"
 # Clone our repo with dotfiles
-if  [ ! -d $dir ] && [ hash git 2>/dev/null ]; then
+if  ! -d $dir && hash git 2>/dev/null; then
     echo "Installing dotfiles repo"
     cd ~
     echo "Clonning dotfiles repo"
@@ -41,14 +41,14 @@ fi
 
 # Install oh my zsh
 echo "SECTION -> zsh"
-if [ ! hash /bin/bash 2>/dev/null ]; then
+if ! hash /bin/bash 2>/dev/null; then
     echo "installing zsh"
     cd ~
     /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi    
 echo "SECTION -> Powerlevel10k"
 # Install Powerlevel10k theme
-if [ hash git 2>/dev/null ]; then
+if hash git 2>/dev/null; then
     echo "installing Powerlevel10k"
     cd ~
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -56,14 +56,14 @@ fi
 
 echo "SECTION -> zsh-autosuggestions"
 # Install zsh-autosuggestions
-if [ hash git 2>/dev/null ]; then
+if hash git 2>/dev/null; then
     echo "Installing zsh-autosuggestions"
     cd ~
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 # Install LTS Node
-if [ hash nvm 2>/dev/null ]; then
+if hash nvm 2>/dev/null; then
     nvm install "lts/*"
 else
     echo "nvm has not been install"
@@ -74,13 +74,13 @@ fi
 files=".p10k.zsh .zshrc .gitconfig"
 
 # Create a dir to move the configuration files there
-if [ ! -d $dir ]; then
+if ! -d $dir; then
     mkdir $dir
 fi   
 
 for file in $files; do
     echo "Creating symlink to $file in home directory."
-    [ -e ~/$file ] && rm ~/$file
+    -e ~/$file && rm ~/$file
     ln -s $dir/$file ~/$file
     source $file
 done
@@ -92,6 +92,6 @@ source .macos
 cd ~
 touch .private-profile
 echo "create temporal file"
-if [ ! -d ~/tmp ]; then
+if ! -d ~/tmp; then
     mkdir ~/tmp
 fi
