fc0bc25dc97558717f3ddb604ed8df05c38ec3bd
Javier Rivas <javier.rivas@devo.com>
2021-07-11 12:00:55 +0200

    error control and tune directories

--- install.sh
+++ install.sh
@@ -1,29 +1,48 @@
 #!bin/bash
 # By FJRivax with love
 
-echo "Starting installation and configuration process"
-echo "Homebrew"
-# Install Homebrew ( package management )
-if [ ! has brew 2>/dev/null ]; then
-    echo "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi    
+echo "Starting installation and configuration process, let's do magic :)"
+# Dotfiles directory
+dir=~/dotfiles
 
+# Create workspace
+echo "create workspace"
+cd ~
+if [ ! -d ~/workspace ]; then
+    mkdir ~/workspace
+fi
+
+echo "Homebrew ( package management )"
+# Install Homebrew ( package management )
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ ! has brew 2>/dev/null ]; then
+    echo "Installing Homebrew packages"
+    brew bundle
+fi    
+# Clone our repo with dotfiles
+if [ ! -d $dir && ! has git 2>/dev/null ]; then
+    cd $dir
+    echo "Clonning dotfiles repo"
+    git clone https://github.com/jrivax/dotfiles.git
+fi    
 # Open Hammerspoon
 # open -a Hammerspoon
 
 # Install oh my zsh
 if [ ! has sh 2>/dev/null ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    cd ~
+    /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi    
 
 # Install Powerlevel10k theme
 if [ ! has git 2>/dev/null ]; then
+    cd ~
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi    
 
 # Install zsh-autosuggestions
 if [ ! has git 2>/dev/null ]; then
+    cd ~
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
@@ -35,7 +54,7 @@ else
 fi        
 
 # Configure profile
-dir=~/dotfiles
+
 files=".p10k.zsh .zshrc .gitconfig"
 
 # Create a dir to move the configuration files there
@@ -54,11 +73,8 @@ done
 source .macos
 
 # Create private-profile
+cd ~
 touch .private-profile
-echo "create workspace"
-if [ ! -d ~/workspace ]; then
-    mkdir ~/workspace
-fi
 echo "create temporal file"
 if [ ! -d ~/tmp ]; then
     mkdir ~/tmp
